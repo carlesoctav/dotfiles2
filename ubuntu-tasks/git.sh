320dea6aa73d403760ed2047b94081cc55f3db91
carlesoctav <carlesoctavianus@tuta.io>
2026-03-20 15:59:01 +0700

    submodule done right ubuntu

--- ubuntu-tasks/git.sh
+++ ubuntu-tasks/git.sh
@@ -1,6 +1,9 @@
-git config --global user.email "carlesoctavianus@tuta.io"
-git config --global user.name "carlesoctav"
 
 sudo apt-add-repository ppa:git-core/ppa
 sudo apt-get update
 sudo apt-get install git
+
+git config --global user.email "carlesoctavianus@tuta.io"
+git config --global user.name "carlesoctav"
+git config --global push.recurseSubmodules on-demand
+git config --global submodule.recurse true
